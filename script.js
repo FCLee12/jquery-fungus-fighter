e163748a7f69c62494de2dedce36ec983203dcd4
Fue Lee <fueclee.12@gmail.com>
2023-02-23 12:00:14 -0600

    added comments for each attack for organization

--- script.js
+++ script.js
@@ -19,4 +19,20 @@ function onReady() {
     // - Handle events that ->
     // - Updates state which is ->
     // - Rendered to the DOM
+
+    // Arcane Sceptre
+    // AP: 12
+    // HP Damage: 14
+
+    // Entangle
+    // AP: 23
+    // HP Damage: 9
+
+    // Dragon Blade
+    // AP: 38
+    // HP Damage: 47
+
+    // Star Fire
+    // AP: 33
+    // HP Damage: 25
 }
