21f5c579f78890ec2dfdb469bc5f82a7ff66da99
Fue Lee <fueclee.12@gmail.com>
2023-02-23 13:25:03 -0600

    created working functions that subtract AP and HP correctly

--- script.js
+++ script.js
@@ -9,6 +9,27 @@ $(document).ready(onReady);
 let fungusHP = 100;
 let attackPower = 100;
 
+// Attack Objects
+const arcaneSceptre = {
+    apCost: 12,
+    hpDamage: 14
+}
+
+const entangle = {
+    apCost: 23,
+    hpDamage: 9
+}
+
+const dragonBlade = {
+    apCost: 38,
+    hpDamage: 47
+}
+
+const starFire = {
+    apCost: 33,
+    hpDamage: 25
+}
+
 function onReady() {
     
     // Make sure you check the index.html file! 
@@ -20,42 +41,42 @@ function onReady() {
     // - Updates state which is ->
     // - Rendered to the DOM
 
-    // Attack Objects
-    let arcaneSceptre = {
-        apCost: 12,
-        hpDamage: 14
-    }
+    //Targets the parent of the attack icons
+    $( '.attacks' ).on('click', '.arcane-sceptre', arcaneSceptreGo)
+    $( '.attacks' ).on('click', '.entangle', entangleGo)
+    $( '.attacks' ).on('click', '.dragon-blade', dragonBladeGo)
+    $( '.attacks' ).on('click', '.star-fire', starFireGo)
 
-    let entangle = {
-        apCost: 23,
-        hpDamage: 9
-    }
+}
 
-    let dragonBlade = {
-        apCost: 38,
-        hpDamage: 47
-    }
+function arcaneSceptreGo() {
+    console.log( 'arcaneSceptreGo is running' );
+    attackPower -= arcaneSceptre.apCost;
+    fungusHP -= arcaneSceptre.hpDamage;
+    console.log( attackPower );
+    console.log( fungusHP );
+}
 
-    let starFire = {
-        apCost: 33,
-        hpDamage: 25
-    }
+function entangleGo() {
+    console.log( 'entangleGo is running' );
+    attackPower -= entangle.apCost;
+    fungusHP -= entangle.hpDamage;
+    console.log( attackPower );
+    console.log( fungusHP );
+}
 
-    
-    // Arcane Sceptre
-    // AP: 12
-    // HP Damage: 14
-    
+function dragonBladeGo() {
+    console.log( 'dragonBladeGo is running' );
+    attackPower -= dragonBlade.apCost;
+    fungusHP -= dragonBlade.hpDamage;
+    console.log( attackPower );
+    console.log( fungusHP );
+}
 
-    // Entangle
-    // AP: 23
-    // HP Damage: 9
-
-    // Dragon Blade
-    // AP: 38
-    // HP Damage: 47
-
-    // Star Fire
-    // AP: 33
-    // HP Damage: 25
+function starFireGo() {
+    console.log( 'starFireGo is running' );
+    attackPower -= starFire.apCost;
+    fungusHP -= starFire.hpDamage;
+    console.log( attackPower );
+    console.log( fungusHP );
 }
